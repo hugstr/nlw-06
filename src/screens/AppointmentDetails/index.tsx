5976ff617842fac4e170eb7c9dd7e81b18dfb554
hugstr <strh93@gmail.com>
2021-06-23 10:04:48 -0300

    ✨ Share button in AppointmentDetails screen

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
+import { BorderlessButton } from "react-native-gesture-handler";
 
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 
 import { styles } from "./styles";
+import { theme } from "../../global/theme";
 
 export function AppointmentDetails() {
   return (
     <Background>
       <Header
         title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto
+              name="share"
+              size={24}
+              color={theme.colors.primary}
+            />
+          </BorderlessButton>
+        }
       />
     </Background>
   )
