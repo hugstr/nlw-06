2bf313e5a4efd00bfd9bc5b18d14e3ed6029cb35
hugstr <strh93@gmail.com>
2021-06-23 11:07:16 -0300

    ✨ Created member componente and added to details

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 import { Fontisto } from "@expo/vector-icons";
+import {
+  ImageBackground,
+  Text,
+  View,
+  FlatList
+} from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 
+import { ListDivider } from "../../components/ListDivider";
+import { ListHeader } from "../../components/ListHeader";
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
+import { Member } from "../../components/Member";
 
 import { styles } from "./styles";
 import { theme } from "../../global/theme";
+import BannerImg from "../../assets/banner.png";
 
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: "1",
+      username: "Hugo",
+      avatar_url: "http://github.com/hugstr.png",
+      status: "online"
+    },
+    {
+      id: "2",
+      username: "Johnson",
+      avatar_url: "http://github.com/nakahwra.png",
+      status: "online"
+    }
+  ]
+
   return (
     <Background>
       <Header
@@ -22,6 +47,35 @@ export function AppointmentDetails() {
             />
           </BorderlessButton>
         }
+      />
+      <ImageBackground
+        source={BannerImg}
+        style={styles.banner}
+      >
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>
+            Lendários
+          </Text>
+
+          <Text style={styles.subtitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+          </Text>
+        </View>
+      </ImageBackground>
+
+      <ListHeader
+        title="Jogadores"
+        subtitle="Total 3"
+      />
+
+      <FlatList
+        data={members}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Member data={item} />
+        )}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.members}
       />
     </Background>
   )
--- src/screens/AppointmentDetails/styles.ts
+++ src/screens/AppointmentDetails/styles.ts
@@ -1,7 +1,33 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../global/theme";
 
 export const styles = StyleSheet.create({
   container: {
-
+    flex: 1
+  },
+  bannerContent: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 24,
+    marginBottom: 30
+  },
+  banner: {
+    width: "100%",
+    height: 234,
+    marginBottom: 30
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading
+  },
+  subtitle: {
+    fontSize: 13,
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.heading
+  },
+  members: {
+    marginLeft: 24,
+    marginTop: 27
   }
-})
+});
