4763a60c78be653f55645297a612434e6778bada
hugstr <strh93@gmail.com>
2021-06-24 11:20:41 -0300

    :construction: WIP aula 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
@@ -9,12 +10,13 @@ void main() {
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,
       title: 'Pay Flow',
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              width: size.width,
+              height: size.height * 0.3,
+              color: AppColors.primary,
+            ),
+            Positioned(
+              top: size.height * 0.1,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                AppImages.person,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
