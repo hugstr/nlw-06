1b230a4be2d3c95e97922dd950d8ba315a04fe26
hugstr <strh93@gmail.com>
2021-06-24 23:22:45 -0300

    🐛 Fixed ModalView

--- src/components/ModalView/index.tsx
+++ src/components/ModalView/index.tsx
@@ -1,33 +1,24 @@
 import React, { ReactNode } from "react";
-import {
-  View,
-  Modal,
-  ModalProps,
-} from "react-native";
+import { View, Modal, ModalProps } from "react-native";
 import { Background } from "../Background";
 
 import { styles } from "./styles";
 
 type Props = ModalProps & {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 export function ModalView({ children, ...rest }: Props) {
   return (
-    <Modal
-      transparent
-      animationType="slide"
-      {...rest}
-    >
+    <Modal transparent animationType="slide" {...rest}>
       <View style={styles.overlay}>
         <View style={styles.container}>
           <Background>
-            <View style={styles.bar}>
-              {children}
-            </View>
+            <View style={styles.bar} />
+            {children}
           </Background>
         </View>
       </View>
     </Modal>
-  )
+  );
 }
