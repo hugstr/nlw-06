72178f3f81445e49e8e9b11ccbabc38e1216cbd0
hugstr <strh93@gmail.com>
2021-06-26 19:00:20 -0300

    ✨ Image in guild list

--- src/components/Guild/index.tsx
+++ src/components/Guild/index.tsx
@@ -3,7 +3,7 @@ import {
   TouchableOpacityProps,
   TouchableOpacity,
   View,
-  Text
+  Text,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
@@ -17,36 +17,26 @@ export type GuildProps = {
   name: string;
   icon: string | null;
   owner: boolean;
-}
+};
 
 type Props = TouchableOpacityProps & {
   data: GuildProps;
-}
+};
 
 export function Guild({ data, ...rest }: Props) {
   return (
-    <TouchableOpacity
-      style={styles.container}
-      activeOpacity={0.7}
-      {...rest}
-    >
-      <GuildIcon />
+    <TouchableOpacity style={styles.container} activeOpacity={0.7} {...rest}>
+      <GuildIcon guildId={data.id} iconId={data.icon} />
       <View style={styles.content}>
         <View>
-          <Text style={styles.title}>
-            {data.name}
-          </Text>
+          <Text style={styles.title}>{data.name}</Text>
           <Text style={styles.type}>
             {data.owner ? "Administrador" : "Convidado"}
           </Text>
         </View>
       </View>
 
-      <Feather
-        name="chevron-right"
-        color={theme.colors.heading}
-        size={24}
-      />
+      <Feather name="chevron-right" color={theme.colors.heading} size={24} />
     </TouchableOpacity>
-  )
+  );
 }
--- src/components/GuildIcon/styles.ts
+++ src/components/GuildIcon/styles.ts
@@ -2,9 +2,17 @@ import { StyleSheet } from "react-native";
 import { theme } from "../../global/theme";
 
 export const styles = StyleSheet.create({
+  container: {
+    width: 62,
+    height: 66,
+    borderRadius: 8,
+    backgroundColor: "#7289da",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
   image: {
     width: 66,
     height: 62,
-    borderRadius: 8,
   },
 });
